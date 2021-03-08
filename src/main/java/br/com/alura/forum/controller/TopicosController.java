fa66b7e56823c8f4846b7d4d3c9894e41e22fff7
fcojosef <fcojosef.o@gmail.com>
2021-03-08 13:15:38 -0300

    Impletando melhorias no methodos cadastra, deletar e consulta.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -2,6 +2,7 @@ package br.com.alura.forum.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
@@ -58,29 +59,36 @@ public class TopicosController {
 	}
 
 	@GetMapping("/{id}")
-	public DetalhesDoTopicoDto detalhar(@PathVariable Long id) {
-		Topico topico = topicoRepository.getOne(id);
-		
-		return new DetalhesDoTopicoDto(topico);
+	public ResponseEntity<DetalhesDoTopicoDto> detalhar(@PathVariable Long id) {
+		Optional<Topico> topico = topicoRepository.findById(id);
+		if(topico.isPresent()) {
+			return ResponseEntity.ok(new DetalhesDoTopicoDto(topico.get()));
+		}
+		return ResponseEntity.notFound().build();
 	}
 	
 	@PutMapping("/{id}")
 	@Transactional
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtulizacaoTopicoForm form ){
-		Topico topico = form.atulizar(id, topicoRepository);
-	
-		return ResponseEntity.ok(new TopicoDto(topico));
+		
+		Optional<Topico> optinal = topicoRepository.findById(id);
+		if(optinal.isPresent()) {
+			Topico topico = form.atulizar(id, topicoRepository);
+			return ResponseEntity.ok(new TopicoDto(topico));
+		}
+		return ResponseEntity.notFound().build();
 	}
 	
 	@DeleteMapping("/{id}")
 	@Transactional
 	public ResponseEntity<?> remover(@PathVariable Long id){
-		
+		Optional<Topico> optinal = topicoRepository.findById(id);
+		if(optinal.isPresent()) {
 		topicoRepository.deleteById(id);
-		
 		return ResponseEntity.ok().build();
+	 }
+		return ResponseEntity.notFound().build();
 	}
-
 }
 
 
